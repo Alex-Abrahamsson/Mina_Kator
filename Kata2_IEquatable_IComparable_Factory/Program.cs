1227a1fb9d8f130ac8d0bef9a62acdc72e4eee94
Martin <martin_lenart@icloud.com>
2022-05-12 13:36:39 +0200

    Kata2 and Kata 5 updated

--- Kata2_IEquatable_IComparable_Factory/Program.cs
+++ Kata2_IEquatable_IComparable_Factory/Program.cs
@@ -7,25 +7,15 @@ var member2 = Member.Factory.CreateWithRandomData();
 Console.WriteLine($"member2: {member2}");
 
 Console.WriteLine("\nCreate a 20 Hilton members");
-var HiltonMembers = MemberList.Factory.CreateWithRandomData(20); 
+var HiltonMembers = MemberList.Factory.CreateWithRandomData(20);
 HiltonMembers.Sort();
-Console.WriteLine($"HiltonMembers Gold Members: {HiltonMembers.NrOfMembers(IsGold)}");
-Console.WriteLine($"HiltonMembers Silver Members: {HiltonMembers.NrOfMembers(IsSilver)}");
-Console.WriteLine($"HiltonMembers Blue Members: {HiltonMembers.NrOfMembers(IsBlueAnd5Years)}");
+Console.WriteLine(HiltonMembers);
 
+Console.WriteLine("\nCreate a 20 Radisson members");
+var RadissonMembers = MemberList.Factory.CreateWithRandomData(20);
+RadissonMembers.Sort();
+Console.WriteLine(RadissonMembers);
 
 Console.WriteLine($"\nHilton member[0]: {HiltonMembers[0]}");
+Console.WriteLine($"Radisson member[0]: {RadissonMembers[0]}");
 Console.WriteLine();
-
-bool IsGold(IMember member) => member.Level == MemberLevel.Gold;
-bool IsSilver(IMember member) => member.Level == MemberLevel.Silver;
-bool IsBlueAnd5Years(IMember member)
-{ 
-    if (member.Level == MemberLevel.Blue)
-    {
-        var ts = DateTime.Now - member.Since;
-        if (ts.Days >= 365 * 5)
-            return true;
-    }
-    return false;
-}
