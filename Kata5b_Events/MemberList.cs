98b07665da799daf03de57242941f5e60e409c07
Martin <martin_lenart@icloud.com>
2022-05-12 14:19:00 +0200

    Kata5b_Events tillagd

---
+++ Kata5b_Events/MemberList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata5_Delegates_Lamda
+{
+    public delegate bool IsMatch(IMember member);
+
+    class MemberList : IMemberList
+    {
+        List<IMember> _members = new List<IMember>();
+
+        public IMember this[int idx]
+        {
+            get { return _members[idx]; }
+        }
+        public int Count() => _members.Count;
+        public int Count(int year)
+        {
+            int c = 0;
+            foreach (var item in _members)
+            {
+                if (item.Since.Year == year)
+                    c++;
+            }
+            return c;
+        }
+
+        public void Sort() => _members.Sort();
+
+        public int NrOfMembers(IsMatch match)
+        {
+            int c = 0;
+            foreach (var item in _members)
+            {
+                if (match(item))
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+
+        public override string ToString()
+        {
+            string sRet = "";
+            for (int i = 0; i < _members.Count; i++)
+            {
+                sRet += $"{_members[i]}\n";
+                if ((i + 1) % 10 == 0)
+                {
+                    sRet += "\n";
+                }
+            }
+            return sRet;
+        }
+
+        #region Class Factory for creating an instance filled with Random data
+        internal static class Factory
+        {
+            internal static MemberList CreateWithRandomData(int NrOfItems, Action<IMember> NewMemberAction)
+            {
+                var memberlist = new MemberList();
+                for (int i = 0; i < NrOfItems; i++)
+                {
+                    var newMember = Member.Factory.CreateWithRandomData();
+                    memberlist._members.Add(newMember);
+
+                    NewMemberAction(newMember);
+                }
+                return memberlist;
+            }
+        }
+        #endregion
+
+        public MemberList() { }
+    }
+}
